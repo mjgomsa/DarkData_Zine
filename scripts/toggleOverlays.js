b5c2342276c8e85b4703c09612688498e8dedc6b
MJ Gomez-Saavedra <majogosaa98@gmail.com>
2023-12-01 20:05:09 -0500

    changes to editors

--- scripts/toggleOverlays.js
+++ scripts/toggleOverlays.js
@@ -1,6 +1,9 @@
-document
-  .getElementById("toggleCheckbox")
-  .addEventListener("change", function () {
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = this.checked ? "block" : "none";
+(function () {
+  var burger = document.querySelector(".burger");
+  var nav = document.querySelector("#" + burger.dataset.target);
+
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
   });
+})();
